8a06b13c8c0b87928e7e0db8cbb7b640b60cc104
Vina Melody <vina_melody@yahoo.com>
2016-09-30 14:44:40 +0800

    Initial Commit

---
+++ fbMessenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fbMessenger
+//
+//  Created by Vina Melody on 30/9/16.
+//  Copyright © 2016 Vina Rianti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
