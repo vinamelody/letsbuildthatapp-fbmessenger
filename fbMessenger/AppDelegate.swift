02a107d4be3139dbb0dc369c0fc2845ec7dc5187
Vina Melody <vina_melody@yahoo.com>
2016-10-03 14:27:59 +0800

    Creating the dummy tab bar items the smart way. Good programmers are always trying to reduce code duplication

--- fbMessenger/AppDelegate.swift
+++ fbMessenger/AppDelegate.swift
@@ -21,9 +21,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        let layout = UICollectionViewFlowLayout()
-        let friendsController = FriendsController(collectionViewLayout: layout)
-        window?.rootViewController = UINavigationController(rootViewController: friendsController)
+//        let layout = UICollectionViewFlowLayout()
+//        let friendsController = FriendsController(collectionViewLayout: layout)
+//        window?.rootViewController = UINavigationController(rootViewController: friendsController)
+//        
+        window?.rootViewController = CustomTabBarController()
         return true
     }
 
---
+++ fbMessenger/CustomTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  CustomTabBarController.swift
+//  fbMessenger
+//
+//  Created by Vina Melody on 3/10/16.
+//  Copyright © 2016 Vina Rianti. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup the friendsController
+        let layout = UICollectionViewFlowLayout()
+        let friendsController = FriendsController(collectionViewLayout: layout)
+        let recentMessagesNavController = UINavigationController(rootViewController: friendsController)
+        recentMessagesNavController.tabBarItem.title = "Recent"
+        recentMessagesNavController.tabBarItem.image = UIImage(named: "recent")
+        
+        
+        viewControllers = [recentMessagesNavController, createDummyNavControllerWithTitle(title: "Calls", imageName: "calls")]
+    }
+    
+    private func createDummyNavControllerWithTitle(title: String, imageName: String) -> UINavigationController {
+        
+        let vc = UIViewController()
+        let navController = UINavigationController(rootViewController: vc)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        
+        return  navController
+    }
+}
